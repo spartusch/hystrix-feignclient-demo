e6d77a9a9cd03fbf61b0ef0be32038ba53fd565a
Stefan Partusch <s.t.partusch@gmail.com>
2017-10-02 17:17:10 +0200

    Update DemoServiceImpl.java

--- src/main/java/com/github/spartusch/hfdemo/service/DemoServiceImpl.java
+++ src/main/java/com/github/spartusch/hfdemo/service/DemoServiceImpl.java
@@ -1,42 +1,52 @@
 package com.github.spartusch.hfdemo.service;
 
-import com.github.spartusch.hfdemo.clients.Decode404FeignClient;
-import com.github.spartusch.hfdemo.clients.DefaultFeignClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.github.spartusch.hfdemo.clients.Decode404FeignClient;
+import com.github.spartusch.hfdemo.clients.ErrorDecoderFeignClient;
+import com.github.spartusch.hfdemo.clients.FallbackFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rx.Single;
 
 @Service
 public class DemoServiceImpl implements DemoService {
 
-    private DefaultFeignClient defaultFeignClient;
+    private FallbackFeignClient fallbackFeignClient;
     private Decode404FeignClient decode404FeignClient;
+    private ErrorDecoderFeignClient errorDecoderFeignClient;
 
     @Autowired
-    public DemoServiceImpl(final DefaultFeignClient defaultFeignClient,
-                           final Decode404FeignClient decode404FeignClient) {
-        this.defaultFeignClient = defaultFeignClient;
+    public DemoServiceImpl(final Decode404FeignClient decode404FeignClient,
+                           final FallbackFeignClient fallbackFeignClient,
+                           final ErrorDecoderFeignClient errorDecoderFeignClient) {
         this.decode404FeignClient = decode404FeignClient;
+        this.fallbackFeignClient = fallbackFeignClient;
+        this.errorDecoderFeignClient = errorDecoderFeignClient;
     }
 
     @Override
-    public String getDataUsingDefaultFeignClient() {
-        return defaultFeignClient.getData();
+    public String getData() {
+        return decode404FeignClient.getData();
     }
 
     @Override
-    @HystrixCommand(fallbackMethod = "getDataUsingDefaultFeignClientFallback")
-    public String getDataUsingDefaultFeignClientWithFallback() {
-        return defaultFeignClient.getData();
+    @HystrixCommand(fallbackMethod = "fallbackMethod")
+    public String getDataWithFallback() {
+        return fallbackFeignClient.getData();
     }
 
-    public String getDataUsingDefaultFeignClientFallback(final Throwable e) {
+    public String fallbackMethod(final Throwable e) {
         return "fallback";
     }
 
     @Override
-    public String getDataUsingDecode404FeignClient() {
-        return decode404FeignClient.getData();
+    public String getDataWithErrorDecoder() {
+        return errorDecoderFeignClient.getData();
+    }
+
+    @Override
+    public Single<String> getRxData() {
+        return decode404FeignClient.getRxData();
     }
 
 }
