12ccb3afde2735f1b10498621c483bee2a86765e
Stefan Partusch <s.t.partusch@gmail.com>
2017-10-03 13:08:19 +0200

    Add error decoder and configure ErrorDecoderFeignClient accordingly

--- src/main/java/com/github/spartusch/hfdemo/clients/ErrorDecoderFeignClient.java
+++ src/main/java/com/github/spartusch/hfdemo/clients/ErrorDecoderFeignClient.java
@@ -1,14 +1,28 @@
 package com.github.spartusch.hfdemo.clients;
 
+import com.github.spartusch.hfdemo.exception.DemoErrorDecoder;
+import feign.codec.ErrorDecoder;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * A feign client configured to use {@link DemoErrorDecoder}.
+ */
 @FeignClient(
-        name = "defaultFeignClient",
-        url = "${clients.url}"
+        name = "errorDecoderFeignClient",
+        url = "${clients.url}",
+        configuration = ErrorDecoderFeignClient.Configuration.class
 )
 public interface ErrorDecoderFeignClient {
+
+    class Configuration {
+        @Bean
+        public ErrorDecoder errorDecoder() {
+            return new DemoErrorDecoder();
+        }
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     String getData();
---
+++ src/main/java/com/github/spartusch/hfdemo/exception/DemoErrorDecoder.java
@@ -0,0 +1,27 @@
+package com.github.spartusch.hfdemo.exception;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.http.HttpStatus;
+
+/**
+ * A simple implementation of feign's {@link ErrorDecoder} interface providing business and technical exceptions.
+ * Feign clients require exceptions to be thrown for HTTP status codes >= 300 which triggers Hystrix fallback/error
+ * behaviour. This error decoder provides {@link BusinessRuntimeException}s for status codes < 500 in order
+ * to avoid Hystrix fallback/error behaviour in these cases. It provides {@link TechnicalRuntimeException}s for
+ * status codes in the 5xx range and thus triggers Hystrix fallback/error behaviour in these cases.
+ */
+public class DemoErrorDecoder implements ErrorDecoder {
+
+    @Override
+    public Exception decode(final String methodKey, final Response response) {
+        final HttpStatus httpStatus = HttpStatus.valueOf(response.status());
+
+        if (httpStatus.is5xxServerError()) {
+            return new TechnicalRuntimeException(response.reason());
+        }
+
+        return new BusinessRuntimeException(response.reason());
+    }
+
+}
